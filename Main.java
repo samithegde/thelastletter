8a783c26242b006bc329a8c6ba5831cf26c98a04
fivna <fionaontario@gmail.com>
2025-05-22 11:12:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public static void printHangman(int wrongGuesses){
         if (wrongGuesses == 0){
             System.out.println("   _____");
@@ -101,15 +104,22 @@ public class Main {
         }
 
         while (!userWon && wrongGuesses < 6){
+            clearScreen();
             printHangman(wrongGuesses);
-            System.out.println();
+
+            System.out.print("Word: ");
             for (int i = 0; i < wordStatus.length; i++){
                 System.out.print(wordStatus[i]);
             }
+            System.out.println();
+            System.out.println();
+
+            System.out.print("Wrong Letters: ");
             for (int i = 0; i < wrongGuesses; i++){
                 System.out.print(wrongLetters[i] + " ");
             }
-
+            System.out.println();
+    
             System.out.print("Your Guess: ");
             String userGuess = input.nextLine().trim().toLowerCase();
 
@@ -120,28 +130,45 @@ public class Main {
                         wordStatus[i] = letterGuess;
                     }
                 }
+                if (!letterFound){
+                    wrongGuesses++;
+                    wrongLetters[wrongGuesses - 1] = letterGuess;
+                }
             }
             else {
                 if (correctWord.equals(userGuess)){
                     userWon = true;
+                    toReturn[0] = 0;
+                    toReturn[1] = wrongGuesses;
+                    return toReturn;
                 }
                 else { 
                     wrongGuesses++;
                 }
             }
+            userWon = hasPlayerWon(wordStatus);
         }
-
-
-
-
-    
+        if (wrongGuesses >= 6){
+                toReturn[0] = 1;
+                toReturn[1] = wrongGuesses;
+        }
+        return toReturn;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-      for(int i = 0; i <= 6; i++)
-        {
-          printHangman(i);
+        String[] forestWords = new String[] {"trees", "moss", "fern", "bark", "pine", "leaf","vines","roots", "birch", "maple", "spruce", "branch","canopy", "forest", "timber", "wildlife", "squirrel", "mushroom", "sapling", "nature", "acorns", "cedar", "jungle", "bamboo", "fallen", "hollow", "thicket", "woodland", "glade", "shade", "lichen", "stump", "treescape", "bushes", "caterpillar", "nest", "badger", "beetle", "cougar", "trail", "grove", "baobab"};
+String[] oceanWords = new String[] {"sea", "marine", "smelt", "coral", "atlantic", "barnacle", "barracuda", "tidal", "shore", "beach", "wave", "aquatic", "seawater", "pacific", "arctic", "antarctic", "sealion", "saltwater", "freshwater", "ocean"};
+	String[] desertWords = new String[] {"desert", "cactus", "sandy", "sahara","sandstone", "pyramid", "quartz", "shrubs", "sandstorm", "dunes", "camel", "vultures", "rattlesnake", "dragonfruit"};
+	String[] coldWords = new String[] {"penguin", "glacier", "frozen", "tundra", "iceberg", "frost", "permafrost", "chill", "arctic", "frigid", "snow", "blizzard"};
+
+        int[] gameResult = playHangman(forestWords);
+        if (gameResult[0] == 0){
+            // user won
         }
+        else {
+            // user lost
+        }
+        int score = gameResult[1];
 
       
     }
