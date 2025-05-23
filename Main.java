7b29e6d9f277d1015549d70551304596afd91f28
fivna <fionaontario@gmail.com>
2025-05-23 10:44:29 -0400

    hihihi

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 import java.util.*;
 
 public class Main {
+    
+    
+    /**
+     * This method clears the console, making the user experience neater.
+     */
     public static void clearScreen(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
+    /**
+     * This method prints the hangman visual based on how many incorrect guesses were already made.
+     * @param wrongGuesses The number of wrong guesses made by the user.
+     */
     public static void printHangman(int wrongGuesses){
         if (wrongGuesses == 0){
             System.out.println("   _____");
@@ -78,7 +88,15 @@ public class Main {
         }
     }
 
+
+    /**
+     * This method checks if all the letters have been revealed.
+     * @param wordStatus Array containing the revealed/unrevealed letters in the word; "_" if unrevealed.
+     * @return True if all the letters have been revealed, false otherwise.
+     */
     public static boolean hasPlayerWon(char[] wordStatus){
+        
+        // The player wins if all letters have been revealed (i.e. none of the characters are "_").
         for (int i = 0; i < wordStatus.length; i++){
             if (wordStatus[i] == '_'){
                 return false;
@@ -87,25 +105,37 @@ public class Main {
         return true;
     }
 
-    public static int[] playHangman(String[] wordSelection){
+    /**
+     * This method simulates a hangman game.
+     * @param wordSelection The array of words to select from, depending on the biome.
+     * @param biome The biome chosen by the user.
+     * @return Use int array to return two values: Index 0 contains win(0) or loss(1), and Index 1 contains user's score.
+     */
+    public static int[] playHangman(String[] wordSelection, String biome){
         Scanner input = new Scanner(System.in);
         
-        int wordIndex = (int)(Math.random() * wordSelection.length);
+        // Get a random number from the array of given words
+        int wordIndex = (int)(Math.random() * wordSelection.length); 
         String correctWord = wordSelection[wordIndex];
-        int[] toReturn = new int[2];
-        int wrongGuesses = 0;
-        char[] wrongLetters = new char[26];
-        char[] wordStatus = new char[correctWord.length()];
-        char letterGuess;
 
+        int[] toReturn = new int[2]; // Index 0 contains win(0) or loss(1); Index 1 contains user's score (number of wrong guesses)
+        char[] wordStatus = new char[correctWord.length()];  // To be printed under the hangman visual
+        char[] wrongLetters = new char[26]; // Bank of incorrect letters
+        int wrongGuesses = 0;
+
+        // Set all the letters to "_", indicating that the letter has not be revealed
         for (int i = 0; i < wordStatus.length; i++){
             wordStatus[i] = '_';
         }
 
+        // Run while there are still unrevealed letters and the user still has guesses
         while (!hasPlayerWon(wordStatus) && wrongGuesses < 6){
+            
             clearScreen();
+            System.out.println("Biome: " + biome + "\n");
             printHangman(wrongGuesses);
 
+            // Print out the revealed letters
             System.out.print("Word: ");
             for (int i = 0; i < wordStatus.length; i++){
                 System.out.print(wordStatus[i]);
@@ -113,18 +143,22 @@ public class Main {
             System.out.println();
             System.out.println();
 
+            // Print out the previously guessed incorrect letters
             System.out.print("Wrong Letters: ");
             for (int i = 0; i < wrongGuesses; i++){
                 System.out.print(wrongLetters[i] + " ");
             }
             System.out.println();
     
+
             System.out.print("Your Guess: ");
-            String userGuess = input.nextLine().trim().toLowerCase();
+            String userGuess = input.nextLine().trim().toLowerCase(); // Remove whitespace and convert to lowercase for consistency
+
 
             if (userGuess.length() == 1){
-                letterGuess = userGuess.charAt(0);
+                char letterGuess = userGuess.charAt(0);
                 boolean letterFound = false;
+
 
                 for (int i = 0; i < correctWord.length(); i++){
                     if (correctWord.charAt(i) == letterGuess){
@@ -133,28 +167,34 @@ public class Main {
                     }
                 }
 
+
                 if (!letterFound){
                     boolean alreadyGuessed = false;
+                    
+                    
                     for (int i = 0; i < wrongGuesses; i++){
                         if (wrongLetters[i] == letterGuess){
                             alreadyGuessed = true;
                         }
                     }
+                    
+
                     if (!alreadyGuessed){
                         wrongGuesses++;
                         wrongLetters[wrongGuesses - 1] = letterGuess;
                     }
-
                 }
-
             }
+
             else {
                 if (correctWord.equals(userGuess)){
                     toReturn[0] = 0;
                     toReturn[1] = wrongGuesses;
+
                     clearScreen();
                     printHangman(wrongGuesses);
                     System.out.println("You win! The word was " + correctWord + ".");
+
                     return toReturn;
                     
                 }
@@ -163,30 +203,33 @@ public class Main {
                 }
             }
         }
+        
         clearScreen();
         printHangman(wrongGuesses);
+
         if (wrongGuesses >= 6){
             toReturn[0] = 1;
             System.out.println("You lost! The word was " + correctWord + ". Better luck next time!");
         }
+
         else {
             System.out.println("You win! The word was " + correctWord + ".");
         }
+
         toReturn[1] = wrongGuesses;
+        
         return toReturn;
     }
-    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] forestWords = new String[] {"trees", "moss", "fern", "bark", "pine", "leaf","vines","roots", "birch", "maple", "spruce", "branch","canopy", "forest", "timber", "wildlife", "squirrel", "mushroom", "sapling", "nature", "acorns", "cedar", "jungle", "bamboo", "fallen", "hollow", "thicket", "woodland", "glade", "shade", "lichen", "stump", "treescape", "bushes", "caterpillar", "nest", "badger", "beetle", "cougar", "trail", "grove", "baobab"};
-        String[] oceanWords = new String[] {"sea", "marine", "smelt", "coral", "atlantic", "barnacle", "barracuda", "tidal", "shore", "beach", "wave", "aquatic", "seawater", "pacific", "arctic", "antarctic", "sealion", "saltwater", "freshwater", "ocean"};
-	    String[] desertWords = new String[] {"desert", "cactus", "sandy", "sahara","sandstone", "pyramid", "quartz", "shrubs", "sandstorm", "dunes", "camel", "vultures", "rattlesnake", "dragonfruit"};
-	    String[] coldWords = new String[] {"penguin", "glacier", "frozen", "tundra", "iceberg", "frost", "permafrost", "chill", "arctic", "frigid", "snow", "blizzard"};
+String[] oceanWords = new String[] {"sea", "marine", "smelt", "coral", "atlantic", "barnacle", "barracuda", "tidal", "shore", "beach", "wave", "aquatic", "seawater", "pacific", "arctic", "antarctic", "sealion", "saltwater", "freshwater", "ocean"};
+	String[] desertWords = new String[] {"desert", "cactus", "sandy", "sahara","sandstone", "pyramid", "quartz", "shrubs", "sandstorm", "dunes", "camel", "vultures", "rattlesnake", "dragonfruit"};
+	String[] coldWords = new String[] {"penguin", "glacier", "frozen", "tundra", "iceberg", "frost", "permafrost", "chill", "arctic", "frigid", "snow", "blizzard"};
 
 
         int highScore = 0;
-        int[] gameResult = playHangman(forestWords);
-        
+        int[] gameResult = playHangman(forestWords, "Forest");
         if (gameResult[0] == 0){
             // user won
         }
